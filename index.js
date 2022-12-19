2fb22c8863ff3890330ee8a2fd8ff0a7513f11c7
phmduc <phmduc1012@gmal.com>
2022-12-19 12:41:51 +0700

    first commit

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 $(".tab-pane").each((index, elem) => {
   let duration;
+  console.log($($(elem).find("video")));
+  if ($(elem).hasClass("active")) {
+  }
   $($(elem).find("video"))[0].onloadedmetadata = function () {
     duration = this.duration;
     const minute = Math.floor(duration / 60);
     const second = Math.floor(duration - minute * 60);
-
     $($($(".nav").find("li")[index]).find("a")).append(
-      `<span class="d-block">${minute}:${second}</span>`
+      `<span class="d-block">${minute}:${
+        second < 10 ? ("0" + second).slice(-2) : second
+      }</span>`
     );
   };
 });
